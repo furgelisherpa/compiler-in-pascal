a6f38714cd9798a8efae9ea3705d7420341a8188
furgeli sherpa <furgelizsherpa@gmail.com>
2026-04-26 17:05:51 +0545

    feat: Implement expression parsing with +, -, *, /, (, ) support

--- src/Cradle.pas
+++ src/Cradle.pas
@@ -1,49 +1,61 @@
+{ Turbo Pascal compatibility mode for the book's code }
 {$mode tp}
 
+{ Cradle: the minimal base compiler/interpreter framework }
 program Cradle;
 
+{ Constant TAB key declaration }
 const TAB = ^I;
 
+{ Lookahead Character }
 var Look: char;
 
+{ Read New Character From Input Stream }
 procedure GetChar;
 begin
   Read(Look);
 end;
 
+{ Report an Error }
 procedure Error(s: string);
 begin
   WriteLn;
-  WriteLn(^G, 'Error: ', s, '.');
+  WriteLn('Error: ', s, '.');
 end;
 
+{ Report Error and Halt }
 procedure Abort(s: string);
 begin
   Error(s);
   Halt;
 end;
 
+{ Report What Was Expected }
 procedure Expected(s: string);
 begin
   Abort(s + ' Expected');
 end;
 
+{ Match a Specific Input Character }
 procedure Match(x: char);
 begin
   if Look = x then GetChar
   else Expected('''' + x + '''');
 end;
 
+{ Recognize an Alpha Character }
 function IsAlpha(c: char): boolean;
 begin
   IsAlpha := upcase(c) in ['A'..'Z'];
 end;
 
+{ Recognize a Decimal Digit }
 function IsDigit(c: char): boolean;
 begin
   IsDigit := c in ['0'..'9'];
 end;
 
+{ Get an Identifier }
 function GetName: char;
 begin
   if not IsAlpha(Look) then Expected('Name');
@@ -51,6 +63,7 @@ begin
   GetChar;
 end;
 
+{ Get a Number }
 function GetNum: char;
 begin
   if not IsDigit(Look) then Expected('Integer');
@@ -58,23 +71,118 @@ begin
   GetChar;
 end;
 
+{ Output a String with Tab }
 procedure Emit(s: string);
 begin
   Write(TAB, s);
 end;
 
+{ Output a String with Tab and CRLF }
 procedure EmitLn(s: string);
 begin
   Emit(s);
   WriteLn;
 end;
 
+{ Parse and Translate a Math Factor }
+procedure Expression; Forward;
+procedure Factor;
+begin
+  if Look = '(' then
+    begin
+      Match('(');
+      Expression;
+      Match(')');
+    end
+  else
+    EmitLn('MOVE #' + GetNum + ',D0');
+end;
+
+{ Recognize and Translate a Multiply }
+procedure Multiply;
+begin
+  Match('*');
+  Factor;
+  EmitLn('MULS (SP)+,D0');
+end;
+
+{ Recognize and Translate a Divide }
+procedure Divide;
+begin
+  Match('/');
+  Factor;
+  { logically its reverse but below code is there just for books reference }
+  { EmitLn('MOVE (SP)+,D1'); }
+  { EmitLn('DIVS D1,D0'); }
+  EmitLn('MOVE (SP)+,D1');
+  EmitLn('DIVS D0,D1');
+  EmitLn('MOVE D1,D0');
+end;
+
+{ Parse and Translate a Math Term }
+procedure Term;
+begin
+  Factor;
+  while Look in ['*', '/'] do
+    begin
+      EmitLn('MOVE D0,-(SP)');
+      case Look of
+        '*': Multiply;
+        '/': Divide;
+      else Expected('Mulop');
+    end;
+  end;
+end;
+
+{ Recognize and Translate an Add }
+procedure Add;
+begin
+  Match('+');
+  Term;
+  EmitLn('ADD (SP)+,D0');
+end;
+
+{ Recognize and Translate a Subtract }
+procedure Subtract;
+begin
+  Match('-');
+  Term;
+  EmitLn('SUB (SP)+,D0');
+  EmitLn('NEG D0');
+end;
+
+{ Recognize an Addop }
+function IsAddop(c: char): boolean;
+begin
+  IsAddop := c in ['+', '-'];
+end;
+
+{ Parse and Translate an Expression }
+procedure Expression;
+begin
+  if IsAddop(Look) then
+    EmitLn('CLR D0')
+  else
+    Term;
+  while Look in ['+', '-'] do
+    begin
+      EmitLn('MOVE D0,-(SP)');
+      case Look of
+      '+': Add;
+      '-': Subtract;
+      else Expected('Addop');
+    end;
+  end;
+end;
+
+{ Initialize }
 procedure Init;
 begin
   GetChar;
-  EmitLn(Look);
 end;
 
+{ Main Program }
 begin
   Init;
+  Expression;
 end.
